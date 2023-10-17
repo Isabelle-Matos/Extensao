0ef8e85ba7419ed1c5ba99cb0bee4fb7de1a12fb
Belle <79115662+Isabelle-Matos@users.noreply.github.com>
2023-10-17 12:44:09 -0300

    Create 1178.c

---
+++ 1178.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+    
+    int a, elev;
+    scanf("%d", &a);
+    for(int i=1; i<=10; i++){
+       printf("%d x %d = %d\n", i, a, (i*a));
+    }
+    
+       
+    
+
+    return 0;
+}
